6e8ae4c2a5caca9d350c860cb1e38c943cc2060b
101zh <67253838+101zh@users.noreply.github.com>
2023-10-13 20:43:20 +0000

    feat(SubnetCalc): can find binary of Network #

--- Subnetting Calculator/SubnetCalc.py
+++ Subnetting Calculator/SubnetCalc.py
@@ -12,6 +12,14 @@ for i in range(4):
     binaryIP[i] = '{0:08b}'.format(int(tempIp[i]))
     binarySubnet[i] = '{0:08b}'.format(int(tempSubnetMask[i]))
 
+binaryNetworkNum=["","","",""]
+for i in range(4):
+    subnetBits = binarySubnet[i]
+    ipBits = binaryIP[i]
+    for j in range(8):
+        binaryNetworkNum[i]+=str(int(ipBits[j:j+1])*int(subnetBits[j:j+1]))
+
 print(binaryIP)
 print(binarySubnet)
+print(binaryNetworkNum)
 
