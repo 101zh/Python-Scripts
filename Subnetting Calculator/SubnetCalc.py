207e4e1eaba2d179f0f3c65b797954861a072949
101zh <67253838+101zh@users.noreply.github.com>
2023-10-13 20:32:03 +0000

    feat(SubnetCalc): can turn subnet mask to binary

--- Subnetting Calculator/SubnetCalc.py
+++ Subnetting Calculator/SubnetCalc.py
@@ -1,9 +1,17 @@
 ipAddress = input("Give a valid IPv4 address: ")
 ipAddress = ipAddress.replace(" ", "")
+subnetMask = input("Give a valid Subnet Mask: ")
+subnetMask = subnetMask.replace(" ", "")
 
 tempIp = ipAddress.split(".")
+tempSubnetMask = subnetMask.split(".")
+
 binaryIP = [0, 0, 0, 0]
+binarySubnet = [0, 0, 0, 0]
 for i in range(4):
     binaryIP[i] = '{0:08b}'.format(int(tempIp[i]))
+    binarySubnet[i] = '{0:08b}'.format(int(tempSubnetMask[i]))
 
+print(binaryIP)
+print(binarySubnet)
 
