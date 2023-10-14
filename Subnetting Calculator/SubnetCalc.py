533d7ee1b1ac9d59b79e269a8b95f9afda8018d0
101zh <67253838+101zh@users.noreply.github.com>
2023-10-14 17:56:18 +0000

    feat(SubnetCalc): finished

--- Subnetting Calculator/SubnetCalc.py
+++ Subnetting Calculator/SubnetCalc.py
@@ -1,25 +1,62 @@
+import math
+
+def formatToAddress(ip : list):
+    ipString = ""
+    for i in range(4):
+        ipString += str(ip[i])+"."
+    ipString = ipString[:-1]
+
+    return ipString
+
 ipAddress = input("Give a valid IPv4 address: ")
 ipAddress = ipAddress.replace(" ", "")
 subnetMask = input("Give a valid Subnet Mask: ")
 subnetMask = subnetMask.replace(" ", "")
 
-tempIp = ipAddress.split(".")
-tempSubnetMask = subnetMask.split(".")
-
-binaryIP = [0, 0, 0, 0]
-binarySubnet = [0, 0, 0, 0]
+binaryIP = ipAddress.split(".")
+binarySubnet = subnetMask.split(".")
 for i in range(4):
-    binaryIP[i] = '{0:08b}'.format(int(tempIp[i]))
-    binarySubnet[i] = '{0:08b}'.format(int(tempSubnetMask[i]))
+    binaryIP[i] = '{0:08b}'.format(int(binaryIP[i]))
+    binarySubnet[i] = '{0:08b}'.format(int(binarySubnet[i]))
 
-binaryNetworkNum=["","","",""]
+binaryNetworkAddress = ["", "", "", ""]
+binaryBroadcastAddress = ["", "", "", ""]
 for i in range(4):
     subnetBits = binarySubnet[i]
     ipBits = binaryIP[i]
     for j in range(8):
-        binaryNetworkNum[i]+=str(int(ipBits[j:j+1])*int(subnetBits[j:j+1]))
+        subnetBit = subnetBits[j:j+1]
+        ipBit = ipBits[j:j+1]
+        binaryNetworkAddress[i] += str(int(ipBit)*int(subnetBit))
+        if (subnetBit == "1"):
+            binaryBroadcastAddress[i] += ipBit
+        else:
+            binaryBroadcastAddress[i] += "1"
 
-print(binaryIP)
-print(binarySubnet)
-print(binaryNetworkNum)
+networkAddress = [0]*4
+broadcastAddress = [0]*4
+totalNumOfHosts= 0
+for i in range(4):
+    networkAddress[i] = int(binaryNetworkAddress[i], 2)
+    broadcastAddress[i] = int(binaryBroadcastAddress[i], 2)
+    # Counts the number of zeroes in a subnet mask
+    totalNumOfHosts+=binarySubnet[i].count("0")
+# 2^# of zeroes is how to find the total number of hosts
+totalNumOfHosts = math.pow(2, totalNumOfHosts)
 
+firstUsableAddress = networkAddress.copy()
+firstUsableAddress[3] += 1
+lastUsableAddress = broadcastAddress.copy()
+lastUsableAddress[3] -= 1
+
+print("_____________________________________________\n")
+print("IPv4 Address: "+ipAddress)
+print("Subnet Mask: "+subnetMask)
+print("Range of IPs: "+formatToAddress(networkAddress) +
+      " - "+formatToAddress(broadcastAddress))
+print("Range of Usable Host IPs: "+formatToAddress(firstUsableAddress) +
+      " - "+formatToAddress(lastUsableAddress))
+print("Total Number of Hosts: "+str(totalNumOfHosts))
+print("Total Number of Usable Hosts: "+str(totalNumOfHosts-2))
+print("Binary Subnet Mask: "+ str(binaryIP))
+print("Binary IP address: "+str(binarySubnet))
